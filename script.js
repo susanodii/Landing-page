33d3bb7ae540252422505dab6d213b1acfd9cef4
susan <odiisusan@gmail.com>
2023-06-14 11:06:20 +0100

    added condition on click of the hamburger

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const navEl = document.querySelector('.nav')
 const hamburgerEl =document.querySelector('.hamburger')
 const heroBaby = document.querySelector('section.hero_baby_background')
-const barEl = document.querySelector('.bar')
+const mainContainer = document.querySelector('.main')
 
 
-hamburgerEl.addEventListener('click', ()=>{
-    heroBaby.style.marginTop = "55rem";
-    navEl.classList.toggle('nav__open')
-    hamburgerEl.classList.toggle('hambuger__open')
+
+
+hamburgerEl.addEventListener('click', () => {
+    // heroBaby.style.marginTop = "55rem";
+    mainContainer.style.display = 'none'
+    navEl.classList.toggle('nav__open');
+    hamburgerEl.classList.toggle('hambuger__open');
     
-})
-
-
+    navEl.addEventListener('transitionend', () => {
+      if (!navEl.classList.contains('nav__open')) {
+        // heroBaby.style.marginTop = "";
+        mainContainer.style.display = 'block'
+      }
+    });
+  });
 
 navEl.addEventListener('click', ()=> {
-    heroBaby.style.marginTop = "0";
+    // heroBaby.style.marginTop = "0";
+    mainContainer.style.display = 'block'
     navEl.classList.remove('nav__open')
     hamburgerEl.classList.remove('hambuger__open')
 })
 
 
-barEl.addEventListener('click',()=> {
-    heroBaby.style.marginTop = "0";
-    hamburgerEl.classList.remove('hambuger__open')
-})
